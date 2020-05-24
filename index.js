6011bcba2e08f34d3120513818a60c3fc2c8a4b1
Heather Empson <hrempson@gmail.com>
2020-05-23 22:36:07 -0700

    github icons & badges

---
+++ index.js
@@ -0,0 +1,8 @@
+$('.github').wrap($('<a>', {
+    href: 'https://github.com/hrempson'
+}));
+
+
+$('.githubicon').click(function(){
+    window.location.assign('https://hrempson.github.io/Password_Generator/');
+});
