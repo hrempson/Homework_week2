276bd06209d1ac8800457af4ffdd26ebbd5ec1b9
Heather Empson <hrempson@gmail.com>
2020-05-25 21:04:38 -0700

    removed unused files

--- index.js
+++
@@ -1,8 +0,0 @@
-$('.github').wrap($('<a>', {
-    href: 'https://github.com/hrempson'
-}));
-
-
-$('.githubicon').click(function(){
-    window.location.assign('https://hrempson.github.io/Password_Generator/');
-});
